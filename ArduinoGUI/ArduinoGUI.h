659f7cfe3e7dc2e92bdac70e8875fcc0faeeb623
John <102992694+johnBamp@users.noreply.github.com>
2023-09-17 01:24:11 -0400

    Create ArduinoGUI.h

---
+++ ArduinoGUI/ArduinoGUI.h
@@ -0,0 +1,9 @@
+#ifndef ARDUINOGUI_H
+#define ARDUINOGUI_H
+
+#include "Widget/Widget.h"
+#include "Menu/Menu.h"
+#include "Button/Button.h"
+#include "ImageDrawer/ImageDrawer.h"
+
+#endif // ARDUINO_GUI_H
